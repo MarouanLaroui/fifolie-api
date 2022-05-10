ea535f79051873b7c55c47a9f5dfee70720d6828
MarouanLaroui <marouanlaroui2000@gmail.com>
2022-05-10 18:40:17 +0200

    fix: taskService fixed the timeouts times.

--- src/tasks/tasks/tasks.service.ts
+++ src/tasks/tasks/tasks.service.ts
@@ -15,7 +15,7 @@ export class TasksService {
       action().catch((error) => console.log(error));
     }
     //timeout of 2mn
-    const interval = setInterval(handleActionExceptions, 20 * 1000);
+    const interval = setInterval(handleActionExceptions, 120 * 1000);
     //const interval = setInterval(action, 120 * 1000);
     this.schedulerRegistry.addInterval('execute-action', interval);
   }
@@ -25,7 +25,7 @@ export class TasksService {
     if (this.schedulerRegistry.doesExist('timeout', 'reset-value')) {
       const action = this.schedulerRegistry.getTimeout('reset-value');
       this.schedulerRegistry.deleteTimeout('reset-value');
-      const timeout = setTimeout(() => action, 120 * 1000);
+      const timeout = setTimeout(() => action, 24 * 60 * 60 * 1000);
       this.schedulerRegistry.addTimeout('reset-value', timeout);
     }
   }
@@ -36,7 +36,7 @@ export class TasksService {
       this.schedulerRegistry.deleteTimeout('reset-value');
     }
     //timeout of 24h
-    const timeout = setTimeout(action, 120 * 1000);
+    const timeout = setTimeout(action, 24 * 60 * 60 * 1000);
     //const timeout = setTimeout(action, 24 * 60 * 60 * 1000);
     this.schedulerRegistry.addTimeout('reset-value', timeout);
   }
